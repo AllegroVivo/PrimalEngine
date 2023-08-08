fcbec70caee517441d1586491c6dc75e79ffae7a
Stephanie Phillips <79615185+AllegroVivo@users.noreply.github.com>
2023-08-08 00:23:37 -0700

    Viode #1 complete. Project Browser is Working.

---
+++ PrimalEditor/GameProject/NewProjectView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PrimalEditor.GameProject;
+
+public partial class NewProjectView : UserControl
+{
+    public NewProjectView()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ PrimalEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace PrimalEditor.GameProject;
+
+public partial class ProjectBrowserDialog : Window
+{
+    public ProjectBrowserDialog()
+    {
+        InitializeComponent();
+    }
+
+    private void OnProjectButton_OnClick(Object sender, RoutedEventArgs e)
+    {
+        if (sender == openProjectButton)
+        {
+            if (newProjectButton.IsChecked == true)
+            {
+                newProjectButton.IsChecked = false;
+                browserContent.Margin = new Thickness(0);
+            }
+
+            openProjectButton.IsChecked = true;
+        }
+        else
+        {
+            if (openProjectButton.IsChecked == true)
+            {
+                openProjectButton.IsChecked = false;
+                browserContent.Margin = new Thickness(-800, 0, 0, 0);
+            }
+
+            newProjectButton.IsChecked = true;
+        }
+    }
+}
+
