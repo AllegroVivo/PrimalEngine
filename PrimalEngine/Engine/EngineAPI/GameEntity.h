31560e02a7bdf80c1dfab60ddd7bd188556293ba
Stephanie Phillips <StephBotCoder@gmail.com>
2024-02-24 22:15:14 -0800

    Video #016 Completed

--- PrimalEngine/Engine/EngineAPI/GameEntity.h
+++ PrimalEngine/Engine/EngineAPI/GameEntity.h
@@ -2,28 +2,85 @@
 
 #include "../Components/ComponentsCommon.h"
 #include "TransformComponent.h"
+#include "ScriptComponent.h"
 
-namespace primal::game_entity
+// ReSharper disable CppClangTidyBugproneMacroParentheses
+
+namespace primal
 {
-    DEFINE_TYPED_ID(entity_id);
-
-    class entity
+    namespace game_entity
     {
-    public:
-        constexpr explicit entity(entity_id id) : _id { id }
+        DEFINE_TYPED_ID(entity_id);
+
+        class entity
         {
+        public:
+            constexpr explicit entity(entity_id id) : _id { id }
+            {
+            }
+
+            constexpr entity() : _id { id::invalid_id }
+            {
+            }
+
+            constexpr entity_id get_id() const { return _id; }
+            constexpr Boolean is_valid() const { return id::is_valid(_id); }
+
+            transform::component transform() const;
+            script::component script() const;
+
+        private:
+            entity_id _id;
+        };
+    }
+
+    namespace script
+    {
+        class entity_script : public game_entity::entity
+        {
+        public:
+            virtual ~entity_script() = default;
+
+            virtual void begin_play()
+            {
+            }
+
+            virtual void update(Single)
+            {
+            }
+
+        protected:
+            constexpr explicit entity_script(game_entity::entity entity)
+                : game_entity::entity { entity.get_id() }
+            {
+            }
+        };
+
+        namespace detail
+        {
+            using script_ptr = std::unique_ptr<entity_script>;
+            using script_creator = script_ptr(*)(game_entity::entity entity);
+            using string_hash = std::hash<std::string>;
+
+            UInt8 register_script(size_t, script_creator);
+
+            template<class script_class>
+            script_ptr create_script(game_entity::entity entity)
+            {
+                assert(entity.is_valid());
+                return std::make_unique<script_class>(entity);
+            }
         }
 
-        constexpr entity() : _id { id::invalid_id }
-        {
+#define REGISTER_SCRIPT(TYPE)                                                   \
+        class TYPE;                                                             \
+        namespace                                                               \
+        {                                                                       \
+            UInt8 _reg_##TYPE {                                                 \
+                primal::script::detail::register_script(                        \
+                    primal::script::detail::string_hash()(#TYPE),               \
+                    &primal::script::detail::create_script<TYPE>)               \
+            };                                                                  \
         }
-
-        constexpr entity_id get_id() const { return _id; }
-        constexpr Boolean is_valid() const { return id::is_valid(_id); }
-
-        transform::component transform() const;
-
-    private:
-        entity_id _id;
-    };
+    }
 }
