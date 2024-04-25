7f4436e5526de29f1f6dbfd80a7280e20f350e67
Stephanie Phillips <StephBotCoder@gmail.com>
2024-04-25 12:22:01 -0700

    Video #8 Complete

---
+++ Editor/Utilities/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Editor.Utilities;
+
+public static class MathUtil
+{
+    public static Single Epsilon => 0.00001f;
+
+    public static Boolean IsTheSameAs(this Single value, Single other) => Math.Abs(value - other) < Epsilon;
+    public static Boolean IsTheSameAs(this Single? value, Single? other)
+    {
+        if (!value.HasValue || !other.HasValue)
+            return false;
+        
+        return Math.Abs(value.Value - other.Value) < Epsilon;
+    }
+}
