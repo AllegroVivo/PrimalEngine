f982dcd31640f87a8ad50e7bcd8b07f52daa4be1
Stephanie Phillips <StephBotCoder@gmail.com>
2024-04-27 09:24:48 -0700

    Video #12 Complete

--- Editor/Dictionaries/ControlTemplates.xaml.cs
+++ Editor/Dictionaries/ControlTemplates.xaml.cs
@@ -32,4 +32,22 @@ public partial class ControlTemplates : ResourceDictionary
             Keyboard.ClearFocus();
         }
     }
+    
+    private void OnClose_Button_Click(Object sender, RoutedEventArgs _)
+    {
+        Window window = (Window)((FrameworkElement)sender).TemplatedParent;
+        window.Close();
+    }
+
+    private void OnMaximizeRestore_Button_Click(Object sender, RoutedEventArgs _)
+    {
+        Window window = (Window)((FrameworkElement)sender).TemplatedParent;
+        window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+    }
+
+    private void OnMinimize_Button_Click(Object sender, RoutedEventArgs _)
+    {
+        Window window = (Window)((FrameworkElement)sender).TemplatedParent;
+        window.WindowState = WindowState.Minimized;
+    }
 }
--- Editor/GameProject/ProjectBrowserDialog.xaml.cs
+++ Editor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Editor.GameProject;
 
 public partial class ProjectBrowserDialog : Window
 {
+    private readonly CubicEase _easing = new() { EasingMode = EasingMode.EaseInOut };
+    
     public ProjectBrowserDialog()
     {
         InitializeComponent();
@@ -30,6 +34,9 @@ public partial class ProjectBrowserDialog : Window
             if (createProjectButton.IsChecked == true)
             {
                 createProjectButton.IsChecked = false;
+                AnimateToOpenProject();
+                newProjectView.IsEnabled = false;
+                openProjectView.IsEnabled = true;
                 browserContent.Margin = new Thickness(0);
             }
 
@@ -40,11 +47,50 @@ public partial class ProjectBrowserDialog : Window
             if (openProjectButton.IsChecked == true)
             {
                 openProjectButton.IsChecked = false;
+                AnimateToCreateProject();
+                newProjectView.IsEnabled = true;
+                openProjectView.IsEnabled = false;
                 browserContent.Margin = new Thickness(-800, 0, 0, 0);
             }
 
             createProjectButton.IsChecked = true;
         }
+    }
+
+    private void AnimateToCreateProject()
+    {
+        DoubleAnimation highlightAnimation = new(200, 400, new Duration(TimeSpan.FromSeconds(0.2)))
+        {
+            EasingFunction = _easing
+        };
+        highlightAnimation.Completed += (_, _) =>
+        {
+            ThicknessAnimation animation = new(new Thickness(0), 
+                new Thickness(-1600, 0, 0, 0), new Duration(TimeSpan.FromSeconds(0.5)))
+            {
+                EasingFunction = _easing
+            };
+            browserContent.BeginAnimation(MarginProperty, animation);
+        };
+        highlightRect.BeginAnimation(Canvas.LeftProperty, highlightAnimation);
+    }
+
+    private void AnimateToOpenProject()
+    {
+        DoubleAnimation highlightAnimation = new(400, 200, new Duration(TimeSpan.FromSeconds(0.2)))
+        {
+            EasingFunction = _easing
+        };
+        highlightAnimation.Completed += (_, _) =>
+        {
+            ThicknessAnimation animation = new(new Thickness(-1600, 0, 0, 0), 
+                new Thickness(0), new Duration(TimeSpan.FromSeconds(0.5)))
+            {
+                EasingFunction = _easing
+            };
+            browserContent.BeginAnimation(MarginProperty, animation);
+        };
+        highlightRect.BeginAnimation(Canvas.LeftProperty, highlightAnimation);
     }
 }
 
